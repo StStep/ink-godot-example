0bf89c02c2948419e1b8764c24bd13c7dcdd7b8a
R. Stephens Summerlin <st.summerlin@gmail.com>
2017-11-23 22:01:06 -0500

    Updated reference frame to reference rect for 3.0

--- addons/GodotTIE/text_interface_engine.gd
+++ addons/GodotTIE/text_interface_engine.gd
@@ -3,7 +3,7 @@
 #(MIT License)
 
 # Intern initializations
-extends ReferenceFrame # Extends from ReferenceFrame
+extends ReferenceRect # Extends from ReferenceRect
 
 const _ARRAY_CHARS = [" ","!","\"","#","$","%","&","'","(",")","*","+",",","-",".","/","0","1","2","3","4","5","6","7","8","9",":",";","<","=",">","?","@","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","[","\\","]","^","_","`","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","{","|","}","~"]
 
