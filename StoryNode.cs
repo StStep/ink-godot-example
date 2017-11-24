29484bfdf17fe8c58666218c58e08b48b20ad39f
R. Stephens Summerlin <st.summerlin@gmail.com>
2017-11-23 22:58:36 -0500

    Added C# script and init mono solution

---
+++ StoryNode.cs
@@ -0,0 +1,23 @@
+using Godot;
+using System;
+
+public class StoryNode : Node
+{
+    // Member variables here, example:
+    // private int a = 2;
+    // private string b = "textvar";
+
+    public override void _Ready()
+    {
+        // Called every time the node is added to the scene.
+        // Initialization here
+
+    }
+
+//    public override void _Process(float delta)
+//    {
+//        // Called every frame. Delta is time since last frame.
+//        // Update game logic here.
+//
+//    }
+}
